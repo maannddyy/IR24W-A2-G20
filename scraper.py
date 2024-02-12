7e6f1d9663c5b6ac201595fbff18181a1a41fbb2
Mandy Yan Feng <mandyf@circinus-23.ics.uci.edu>
2024-02-12 03:17:38 -0800

    added tokenizer

--- scraper.py
+++ scraper.py
@@ -3,8 +3,6 @@ from urllib.parse import urlparse, urldefrag
 import logging
 from bs4 import BeautifulSoup
 
-import requests
-
 STOP_WORDS = {"a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
 token_frequencies = dict()  # frequencies of all tokens excluding stop words
 longest_file = 0
@@ -32,10 +30,9 @@ def extract_next_links(url, resp):
         return list()
 
     soup = BeautifulSoup(resp.raw_response.content, "html.parser")
+    content = soup.get_text()
 
-
-    if len(resp.raw_response.content)
-
+    tokens = tokenize(content)
 
     all_links = [link['href'] for link in soup.find_all('a', href=True)]
     valid_links = list()
@@ -47,6 +44,21 @@ def extract_next_links(url, resp):
             visited_urls.add(defrag_link)
 
     return list()
+
+
+def tokenize(file_name):
+    file = open(file_name, "r")
+    content = file.read()
+
+    tokens = re.findall(r'[a-z0-9]+', content.lower())
+
+    file.close()
+    
+    # add tokens to frequencies dict
+    for token in tokens:
+        if token not in STOP_WORDS:
+            token_frequences[token] = token_frequencies.get(token, 0) + 1
+
 
 def is_trap():
     pass
