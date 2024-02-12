7a7ab92effbf43e2068ad243eeee1ab3e3f16421
Mandy Yan Feng <mandyf@circinus-23.ics.uci.edu>
2024-02-12 03:32:48 -0800

    check for longest page

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 STOP_WORDS = {"a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
 token_frequencies = dict()  # frequencies of all tokens excluding stop words
-longest_file = 0
+longest_page = ["ics.uci.edu", 0]
 subdomain_frequencies = dict()  # ics.uci.edu subdomains
 visited_urls = set()
 
@@ -29,11 +29,17 @@ def extract_next_links(url, resp):
     if resp.status != 200:
         return list()
 
+
     soup = BeautifulSoup(resp.raw_response.content, "html.parser")
     content = soup.get_text()
 
-    tokens = tokenize(content)
+    num_tokens = tokenize(content)
 
+    # checks for longest page
+    if num_tokens > longest_page[1]:
+        longest_page = [url, num_tokens]
+
+    # extract links using parsed beautiful soup content
     all_links = [link['href'] for link in soup.find_all('a', href=True)]
     valid_links = list()
 
@@ -43,7 +49,7 @@ def extract_next_links(url, resp):
             valid_links.append(defrag_link)
             visited_urls.add(defrag_link)
 
-    return list()
+    return valid_links
 
 
 def tokenize(file_name):
@@ -55,10 +61,12 @@ def tokenize(file_name):
     file.close()
     
     # add tokens to frequencies dict
+    frequencies = dict()
     for token in tokens:
         if token not in STOP_WORDS:
-            token_frequences[token] = token_frequencies.get(token, 0) + 1
-
+            token_frequences[token] = token_frequencies.get(token, 0) + 1   # records frequencies of tokens of all pages crawled
+    
+    return len(tokens)
 
 def is_trap():
     pass
