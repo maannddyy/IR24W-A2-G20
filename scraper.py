d6e5f2731dfcc8088d16f7d24f8578d6c5aab6e6
Jacqueline Ramirez <jacqurr2@uci.edu>
2024-02-12 22:08:43 -0800

    added subdomain tracking

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import re
 from urllib.parse import urlparse, urldefrag
 import logging
 from bs4 import BeautifulSoup
+import requests
 
 
 STOP_WORDS = {"a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
@@ -43,6 +44,7 @@ def extract_next_links(url, resp):
     num_tokens = tokenize(content)
 
     # checks for longest page
+    global longest_page
     if num_tokens > longest_page[1]:
         longest_page = [url, num_tokens]
 
@@ -56,22 +58,28 @@ def extract_next_links(url, resp):
             valid_links.append(defrag_link)
             visited_urls.add(defrag_link)
 
+            #keep track of subdomains
+            subdomain = get_subdomains(defrag_link)
+            subdomain_frequencies[subdomain] = subdomain_frequencies.get(subdomain, 0)+ 1
+
+
+
     return valid_links
 
 
-def tokenize(file_name):
-    file = open(file_name, "r")
-    content = file.read()
+def tokenize(content):
+    # file = open(file_name, "r")
+    # content = file.read()
 
     tokens = re.findall(r'[a-z0-9]+', content.lower())
 
-    file.close()
+    # file.close()
     
     # add tokens to frequencies dict
     frequencies = dict()
     for token in tokens:
         if token not in STOP_WORDS:
-            token_frequences[token] = token_frequencies.get(token, 0) + 1   # records frequencies of tokens of all pages crawled
+            token_frequencies[token] = token_frequencies.get(token, 0) + 1   # records frequencies of tokens of all pages crawled
     
     return len(tokens)
 
@@ -104,6 +112,13 @@ def is_valid(url):
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
+def get_subdomains(url):
+    parsed_url = urlparse(url)
+    subdomain_parts = parsed_url.netloc.split('.')
+    if len(subdomain_parts) >=3:
+        return "."join(subdomain_parts[-3:])
+    return parsed_url.netloc
 
 def report():
     print("LONGEST PAGE:", longest_page)
