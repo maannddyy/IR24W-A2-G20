c53129690c11092d32d3d4363022100d5edfdfd7
Mandy Yan Feng <mandyf@circinus-31.ics.uci.edu>
2024-02-13 00:21:39 -0800

    minor changes

--- scraper.py
+++ scraper.py
@@ -2,7 +2,6 @@ import re
 from urllib.parse import urlparse, urldefrag
 import logging
 from bs4 import BeautifulSoup
-import requests
 
 
 STOP_WORDS = {"a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
@@ -37,9 +36,9 @@ def extract_next_links(url, resp):
     if resp.status != 200:
         return list()
 
-    redir_resp = detect_redirects(resp.url)
-    if redir_resp is not None:
-        resp = redir_resp
+    # redir_resp = detect_redirects(resp.url)
+    # if redir_resp is not None:
+    #     resp = redir_resp
 
 
     soup = BeautifulSoup(resp.raw_response.content, "html.parser")
@@ -52,6 +51,14 @@ def extract_next_links(url, resp):
     if num_tokens > longest_page[1]:
         longest_page = [url, num_tokens]
 
+    #keep track of subdomains
+    parsed_netloc = parsed.netloc.split('.')
+    if len(parsed_netloc) > 3 and ".".join(parsed_netloc[-3:]) == "ics.uci.edu":
+        subdomain_frequencies[parsed_netloc] = subdomain_frequencies.get(parsed_netloc, 0) + 1
+
+    # subdomain = get_subdomains(defrag_link)
+    # subdomain_frequencies[subdomain] = subdomain_frequencies.get(subdomain, 0)+ 1
+
     # extract links using parsed beautiful soup content
     all_links = [link['href'] for link in soup.find_all('a', href=True)]
     valid_links = list()
@@ -62,9 +69,7 @@ def extract_next_links(url, resp):
             valid_links.append(defrag_link)
             visited_urls.add(defrag_link)
 
-            #keep track of subdomains
-            subdomain = get_subdomains(defrag_link)
-            subdomain_frequencies[subdomain] = subdomain_frequencies.get(subdomain, 0)+ 1
+            
 
     return valid_links
 
@@ -122,20 +127,20 @@ def get_subdomains(url):
         return ".".join(subdomain_parts[-3:])
     return parsed_url.netloc
 
-def detect_redirects(url):
-    # Detect redirects and if the page redirects your crawler, index the redirected content
-    try:
-        response = requests.get(url, allow_redirects=True)
-        redir_url = response.url
-        if redir_url != url:
-            print("redirect detected to:", redir_url)
-        else:
-            print("no redirects")
+# def detect_redirects(url):
+#     # Detect redirects and if the page redirects your crawler, index the redirected content
+#     try:
+#         response = requests.get(url, allow_redirects=True)
+#         redir_url = response.url
+#         if redir_url != url:
+#             print("redirect detected to:", redir_url)
+#         else:
+#             print("no redirects")
 
-        return response
-    except requests.exceptions.RequestException as e:
-        print("error:", e)
-        return None
+#         return response
+#     except requests.exceptions.RequestException as e:
+#         print("error:", e)
+#         return None
 
 
 
@@ -145,3 +150,4 @@ def report():
     
 
 if __name__ == "__main__":
+    pass
